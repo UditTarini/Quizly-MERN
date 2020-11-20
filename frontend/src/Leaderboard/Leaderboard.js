866218a3b244984b7cf9774d189e5c9c72c1f841
UditTarini <udit.tarini937@gmail.com>
2020-11-20 21:32:48 +0530

    marker on leaderboard

--- frontend/src/Leaderboard/Leaderboard.js
+++ frontend/src/Leaderboard/Leaderboard.js
@@ -2,15 +2,34 @@ import React, {useEffect, useState} from "react";
 import "./Leaderboard.css";
 import NavigationBar from "../Commons/NavigationBar";
 import {loadLeaderboard} from "../Commons/Utils/apiHelper";
+import {isAuthenticated} from "../Commons/Utils/authHelper";
 
 const Leaderboard = () => {
   const [data, setData] = useState([]);
+  const [isLoading, setisLoading] = useState(true);
+
+  const {user} = isAuthenticated();
 
   useEffect(() => {
     loadLeaderboard().then((data) => {
       setData(data);
+      setisLoading(false);
     });
   }, []);
+
+  const tableRow = () => {
+    return data.map((item, index) => {
+      return (
+        <tr key={index}>
+          <th scope="row">{`${index + 1} ${
+            item.name == user.name ? ">>" : ""
+          }`}</th>
+          <td>{item.name}</td>
+          <td>{item.totalscore}</td>
+        </tr>
+      );
+    });
+  };
 
   return (
     <>
@@ -26,16 +45,11 @@ const Leaderboard = () => {
               </tr>
             </thead>
             <tbody>
-              {console.log(data)}
-              {data.map((item, index) => {
-                return (
-                  <tr key={index}>
-                    <th scope="row">{index + 1}</th>
-                    <td>{item.name}</td>
-                    <td>{item.totalscore}</td>
-                  </tr>
-                );
-              })}
+              {isLoading ? (
+                <i className="fas fa-spinner fa-pulse loader"></i>
+              ) : (
+                tableRow()
+              )}
             </tbody>
           </table>
         </div>
@@ -43,5 +57,7 @@ const Leaderboard = () => {
     </>
   );
 };
-
+{
+  /* <i class="fas fa-spinner fa-pulse"></i> */
+}
 export default Leaderboard;
